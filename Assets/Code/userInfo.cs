e1780ae9657c275dbd5fc8a2a4ee0f5bef0d0784
USER <judyjry0127@gmail.com>
2021-01-03 21:10:10 +0800

    main

---
+++ Assets/Code/userInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class userInfo : MonoBehaviour
+{
+    public bool bearHead = false;
+    public bool bearRightHand = false;
+    public bool bearLeftHand = false;
+    public bool bearLeg = false;
+    public bool bearBody = false;
+    public bool treasureMap = false;
+    public bool safeBox = false;
+    public Text promptText;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
