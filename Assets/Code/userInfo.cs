e0fdb14f1e0ab2008c0c6f1dd14fcd38bad9d0e8
JudyJry <judyjry0127@gmail.com>
2021-01-04 19:27:06 +0800

    userInfo

--- Assets/Code/userInfo.cs
+++ Assets/Code/userInfo.cs
@@ -12,8 +12,10 @@ public class userInfo : MonoBehaviour
     public bool bearBody = false;
     public bool treasureMap = false;
     public bool safeBox = false;
-    public Text promptText;
+    public Text collectText;
+    public Text timeText;
     public float time;
+    private int limitTime = 10*60; //10min*60s
 
     void Start()
     {
@@ -24,27 +26,42 @@ public class userInfo : MonoBehaviour
     void Update()
     {
         time+=Time.deltaTime;
+        collect();
+        timeChange();
+    }
+
+    void collect(){
         if (treasureMap){
-            promptText.text = "已收集：";
+            collectText.text = "已收集：";
             if (bearHead){
-                promptText.text += " 頭 ";
+                collectText.text += " 頭 ";
             }
             if (bearRightHand){
-                promptText.text += " 右手 ";
+                collectText.text += " 右手 ";
             }
             if (bearLeftHand){
-                promptText.text += " 左手 ";
+                collectText.text += " 左手 ";
             }
             if (bearLeg){
-                promptText.text += " 腳 ";
+                collectText.text += " 腳 ";
             }
             if (bearBody){
-                promptText.text += " 身體 ";
+                collectText.text += " 身體 ";
             }
         }
         else 
         {
-            promptText.text = "";
+            collectText.text = "";
+        }
+    }
+
+    void timeChange(){
+        int lastTime = limitTime-(int)time;
+        int min = lastTime/60;
+        int sec = lastTime-min*60;
+        timeText.text = string.Format("剩餘時間：{0:D2}:{1:D2}",min,sec);
+        if (lastTime<=60) {
+            timeText.color = Color.red;
         }
     }
 }
