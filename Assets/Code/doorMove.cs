5338d82dc6b078e302c2b155151074925b470d2f
JudyJry <judyjry0127@gmail.com>
2021-01-05 00:12:07 +0800

    mian

---
+++ Assets/Code/doorMove.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.Vive;
+
+
+public class doorMove : MonoBehaviour
+{
+    public bool zMirror = false;
+    private bool isTrigger = false;
+    private bool isPressedR = false;
+    private bool isPressedL = false;
+    public userInfo user;
+    private GameObject rh, lh;
+    private float zm = 1;
+
+    void Start()
+    {
+        rh = user.rightHand;
+        lh = user.leftHand;
+        if (zMirror) zm = -zm;
+    }
+
+
+    void Update()
+    {
+        isPressedR = ViveInput.GetPress(HandRole.RightHand, ControllerButton.Trigger);
+        //if (isPressedR) Debug.Log("isPressedR");
+        isPressedL = ViveInput.GetPress(HandRole.LeftHand, ControllerButton.Trigger);
+        //if (isPressedL) Debug.Log("isPressedL");
+        if (isTrigger && isPressedR)
+        {
+            if (isOpen(rh))
+            {
+                transform.Rotate(0, 90 * zm * Time.deltaTime, 0);
+            }
+            else
+            {
+                transform.Rotate(0, -90 * zm * Time.deltaTime, 0);
+            }
+        }
+        else if (isTrigger && isPressedL)
+        {
+            if (isOpen(lh))
+            {
+                transform.Rotate(0, 90 * zm * Time.deltaTime, 0);
+            }
+            else
+            {
+                transform.Rotate(0, -90 * zm * Time.deltaTime, 0);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        isTrigger = true;
+        //Debug.Log("isTrigger");
+    }
+    private void OnTriggerExit(Collider collider)
+    {
+        isTrigger = false;
+    }
+
+    private bool isOpen(GameObject hand)
+    {
+        if (hand.transform.position.z - transform.position.z > 0) { return true; }
+        else if (hand.transform.position.z - transform.position.z < 0) { return false; }
+        return false;
+    }
+
+}
--- Assets/Code/userInfo.cs
+++ Assets/Code/userInfo.cs
@@ -15,53 +15,66 @@ public class userInfo : MonoBehaviour
     public Text collectText;
     public Text timeText;
     public float time;
-    private int limitTime = 10*60; //10min*60s
+    private int limitTime = 10 * 60; //10min*60s
+
+    public GameObject rightHand;
+    public GameObject leftHand;
 
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
-        time+=Time.deltaTime;
+        time += Time.deltaTime;
         collect();
         timeChange();
     }
 
-    void collect(){
-        if (treasureMap){
+    void collect()
+    {
+        if (treasureMap)
+        {
             collectText.text = "已收集：";
-            if (bearHead){
+            if (bearHead)
+            {
                 collectText.text += " 頭 ";
             }
-            if (bearRightHand){
+            if (bearRightHand)
+            {
                 collectText.text += " 右手 ";
             }
-            if (bearLeftHand){
+            if (bearLeftHand)
+            {
                 collectText.text += " 左手 ";
             }
-            if (bearLeg){
+            if (bearLeg)
+            {
                 collectText.text += " 腳 ";
             }
-            if (bearBody){
+            if (bearBody)
+            {
                 collectText.text += " 身體 ";
             }
         }
-        else 
+        else
         {
             collectText.text = "";
         }
     }
 
-    void timeChange(){
-        int lastTime = limitTime-(int)time;
-        int min = lastTime/60;
-        int sec = lastTime-min*60;
-        timeText.text = string.Format("剩餘時間：{0:D2}:{1:D2}",min,sec);
-        if (lastTime<=60) {
+    void timeChange()
+    {
+        int lastTime = limitTime - (int)time;
+        int min = lastTime / 60;
+        int sec = lastTime - min * 60;
+        timeText.text = string.Format("剩餘時間：{0:D2}:{1:D2}", min, sec);
+        if (lastTime <= 60)
+        {
             timeText.color = Color.red;
         }
     }
+
 }
