acd85cf76f5cec7241e10013c11195d5c6e97a20
JudyJry <judyjry0127@gmail.com>
2021-01-04 19:52:27 +0800

    mian

---
+++ Assets/Code/userMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HTC.UnityPlugin.VRModuleManagement;
+using HTC.UnityPlugin.Vive;
+
+public class userMove : MonoBehaviour
+{
+    public float speed = 10;
+    void Start()
+    {
+        var RightHand = ViveRole.GetDeviceIndex(HandRole.RightHand);
+        if(!VRModule.IsValidDeviceIndex(RightHand)) return;
+        
+    }
+
+    void Update()
+    {
+        Vector2 vector = ViveInput.GetPadTouchAxis(HandRole.RightHand);
+        Vector3 direction = new Vector3(vector.x,0,vector.y);
+        if (direction!=Vector3.zero){
+            transform.Translate(direction*Time.deltaTime*speed);
+        }
+    }
+}
