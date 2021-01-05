afc06a7f9bad139b97139f508f27ba0f96d11e13
JudyJry <judyjry0127@gmail.com>
2021-01-05 14:51:39 +0800

    main

---
+++ Assets/Code/Dead2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead2 : MonoBehaviour
+{
+    public GameObject mc;
+    
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        transform.position = mc.transform.position;
+        transform.rotation = mc.transform.rotation;
+    }
+}
---
+++ Assets/Code/frame4.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class frame4 : MonoBehaviour
+{
+    public GameObject deadUi;
+    public Material normal, die;
+    public float time = 0;
+    private bool isWatch = false;
+
+    void Start()
+    {
+
+    }
+
+    
+    void Update()
+    {
+        if (isWatch) {
+            time+=Time.deltaTime;
+            if (time > 3 ){
+                dieVision();
+                StartCoroutine(die2());
+            }
+        } 
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Vision")
+        {
+            isWatch = true;
+        }
+
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        time = 0;
+        isWatch = false;
+    }
+
+    public void dieVision(){
+        gameObject.GetComponent<AudioSource>().enabled = true;
+        gameObject.GetComponent<Renderer>().material = die;
+    }
+
+     public IEnumerator die2()
+    {
+        yield return new WaitForSeconds(0.5f);
+        deadUi.SetActive(true);
+    }
+}
