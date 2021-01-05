cd9e1f65d8cc6fb910599d3ce7d81d5a4d8e43b0
JudyJry <judyjry0127@gmail.com>
2021-01-05 15:11:26 +0800

    main

---
+++ Assets/Code/Dead3.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dead3 : MonoBehaviour
+{
+     public GameObject mc;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.position = mc.transform.position;
+    }
+}
---
+++ Assets/Code/safeBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class safeBox : MonoBehaviour
+{
+    public GameObject deadUi;
+    public GameObject pp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Body")
+        {
+            pp.SetActive(true);
+            StartCoroutine(die());
+        }
+
+    }
+
+    public IEnumerator die() {
+        yield return new WaitForSeconds(0.5f);
+        deadUi.SetActive(true);
+    }
+}
